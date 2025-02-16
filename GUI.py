e263b2f59940fd8199cc78de6ad4e59f82759dce
atomiczagnut <atomiczagnut@gmail.com>
2025-02-16 03:38:36 -0700

    Updated readme and fixed some typos

---
+++ GUI.py
@@ -0,0 +1,30 @@
+# The functions which control the GUI
+
+def draw_fretboard(canvas):
+	canvas.create_line(50, 50, 50, 450)
+	canvas.create_line(100, 50, 100, 450)
+	canvas.create_line(150, 50, 150, 450)
+	canvas.create_line(200, 50, 200, 450)
+	canvas.create_line(50, 50, 200, 50, width = 5)
+	canvas.create_line(50, 100, 200, 100)
+	canvas.create_line(50, 150, 200, 150)
+	canvas.create_line(50, 200, 200, 200)
+	canvas.create_line(50, 250, 200, 250)
+	canvas.create_line(50, 300, 200, 300)
+	canvas.create_line(50, 350, 200, 350)
+	canvas.create_line(50, 400, 200, 400)
+	canvas.create_line(50, 450, 200, 450)
+
+def draw_note(x, y, canvas):
+	x0 = x - 10
+	y0 = y - 10
+	x1 = x + 10
+	y1 = y + 10
+	if y < 50:
+		canvas.create_oval(x0, y0, x1, y1) # Open circle indicates open string
+	else:
+		canvas.create_oval(x0, y0, x1, y1, fill = "black") # Filled circle indicates finger
+
+def dropdown_menu(canvas):
+	pass
+
---
+++ notes_and_scales.py
@@ -0,0 +1,36 @@
+# The collections which define the notes and scales
+
+# Dictionary that defines the scales
+# I used standard MIDI notes (60 = middle C)
+
+# TODO: Add functionality for other scales
+
+scaleDict ={ 
+	"C Major": [60, 62, 64, 65, 67, 69, 71, 72],
+	"C Minor": [60, 62, 63, 65, 67, 68, 70, 72],
+	"C Blues": [60, 63, 65, 66, 67, 71, 72],
+	"C Spanish": [60, 61, 64, 65, 67, 68, 70, 72],
+}
+
+# Dictionary that stores x, y values of notes
+# TODO: The high G string will require its own functionality
+
+noteDict = {
+	60: (100, 25),
+	61: (100, 100),
+	62: (100, 150),
+	63: (100, 200),
+	64: (150, 25),
+	65: (150, 100),
+	66: (150, 250),
+	67: (150, 200),
+	68: (150, 250),
+	69: (200, 25),
+	70: (200, 100),
+	71: (200, 150),
+	72: (200, 200),
+	73: (200, 250),
+	74: (200, 300),
+	75: (200, 350),
+	76: (200, 400),
+}
